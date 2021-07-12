cb38dae09880f50514794b02bbea15d071d48a0d
W. Ross Morrow <rmorrow@metromile.com>
2021-07-12 08:43:51 -0700

    toplevel check

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-for K in */kustomization.yaml ; do 
+BASEDIR=$( git rev-parse --show-toplevel )
+for K in ${BASEDIR}/*/kustomization.yaml ; do 
     kustomize build $( dirname $K ) 1> /dev/null
 done
